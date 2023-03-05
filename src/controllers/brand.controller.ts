41bd207737db8e5bb6405eba090fdab171bfa6b1
Sebastian009w <ia3266505@gmail.com>
2023-03-05 10:30:04 -0500

    endspoints nuevos --

--- src/controllers/brand.controller.ts
+++ src/controllers/brand.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('brand')
 export class BrandController {
   @Get()
   getBrand() {
     return 'Obteniendo brand';
+  }
+
+  @Post()
+  create(@Body() playload) {
+    return {
+      message: 'creando una marca',
+      playload,
+    };
   }
 }
