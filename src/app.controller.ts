51ab9ee0d0fd6602a6d4f3c150537dc83fbe178f
Sebastian009w <ia3266505@gmail.com>
2023-03-05 11:30:43 -0500

    manejos de codigo de estados --

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return 'Hello NestJS';
+  getHello() {
+    return {
+      message: 'Hello nestJs',
+      app: this.appService.getHello(),
+    };
   }
 }
